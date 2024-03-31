ac0d446790a0b586ccba497c1ccc272d960c71fe
Colin Lynch <clynch45@uwo.ca>
2024-03-31 12:58:08 -0400

    All java docs done

--- src/SoundPlayer.java
+++ src/SoundPlayer.java
@@ -2,9 +2,19 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The SoundPlayer class provides methods to play sound files using the Java Sound API.
+ */
 public class SoundPlayer {
+
     private Clip clip;
 
+    /**
+     * Plays the sound file specified by the file path.
+     *
+     * @param filePath the path to the sound file to be played
+     * @param loop     true if the sound should loop continuously, false otherwise
+     */
     public void playSound(String filePath, boolean loop) {
         try {
             // Open an audio input stream.
@@ -29,6 +39,9 @@ public class SoundPlayer {
         }
     }
 
+    /**
+     * Stops the currently playing sound, if any.
+     */
     public void stopSound() {
         if (clip != null) {
             clip.stop();
